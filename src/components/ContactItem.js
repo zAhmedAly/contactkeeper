ae5078c2499d7d27a3ec6b887966e4188f604e30
Ahmed Aly <aaoflaca@gmail.com>
2021-02-14 23:11:50 -0800

    adjust type badge

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,64 +1,66 @@
-import React from "react";
-import { Badge, Button, Card } from "react-bootstrap";
-import { FaPhone, FaEnvelopeOpen } from "react-icons/fa";
+import React from 'react';
+import { Badge, Button, Card } from 'react-bootstrap';
+import { FaPhone, FaEnvelopeOpen } from 'react-icons/fa';
 
 const ContactItem = ({ contact }) => {
   return (
     <Card
       style={{
-        margin: "0.5rem 0",
-        padding: "0.5rem 1rem",
-        backgroundColor: "#f4f4f4",
+        margin: '0.5rem 0',
+        padding: '0.5rem 1rem',
+        backgroundColor: '#f4f4f4',
       }}
     >
       {/* <Card.Body> */}
-      <Card.Title style={{ marginBottom: "0" }}>
+      <Card.Title style={{ marginBottom: '0' }}>
         <h5
           style={{
-            color: "darkblue",
-            fontWeight: "bold",
-            alignContent: "center",
-            justifyContent: "center",
-            margin: "0.5rem 0",
+            color: 'darkblue',
+            fontWeight: 'bold',
+            alignContent: 'center',
+            justifyContent: 'center',
+            margin: '0.5rem 0',
           }}
         >
-          {contact.name}{" "}
+          {contact.name}{' '}
           <Badge
             style={{
-              float: "right",
-              padding: "0.3rem 0.7rem",
-              textAlign: "center",
-              alignContent: "center",
-              justifyContent: "center",
-              margin: "0 0.3rem",
-              borderRadius: "5px",
-              height: "1.5rem",
+              float: 'right',
+              padding: 'auto 0.7rem',
+              textAlign: 'center',
+              alignContent: 'center',
+              justifyContent: 'center',
+              margin: '0 0.3rem',
+              borderRadius: '5px',
+              height: '1.3rem',
+              fontSize: '0.8rem',
+              verticalAlign: 'middle',
             }}
-            variant={contact.type === "professional" ? "success" : "primary"}
+            variant={contact.type === 'professional' ? 'success' : 'primary'}
           >
             {contact.type.charAt(0).toUpperCase() + contact.type.slice(1)}
           </Badge>
         </h5>
       </Card.Title>
 
-      <Card.Text style={{ marginBottom: "0.5rem" }}>
+      <Card.Text style={{ marginBottom: '0.5rem' }}>
         <FaEnvelopeOpen /> {contact.email}
       </Card.Text>
-      <Card.Text style={{ marginBottom: "0.5rem" }}>
+      <Card.Text style={{ marginBottom: '0.5rem' }}>
         <FaPhone /> {contact.phone}
       </Card.Text>
-      <div style={{ marginBottom: "0.5rem" }}>
+      <div style={{ marginBottom: '0.5rem' }}>
         <Button
-          variant="primary"
-          size="sm"
-          style={{ margin: "0 0.2rem" }}
+          variant='primary'
+          size='sm'
+          style={{ margin: '0 0.2rem' }}
           // onClick={() => setContactForEdit(contact)}
         >
           <strong>Edit</strong>
         </Button>
         <Button
-          variant="danger"
-          size="sm"
+          variant='danger'
+          size='sm'
           // onClick={() => {
           //   deleteContact(contact.id);
           // }}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,77 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import {
   FaUserPlus,
   FaAddressBook,
   FaSignInAlt,
   FaSignOutAlt,
   FaHome,
-} from "react-icons/fa";
-import { LinkContainer } from "react-router-bootstrap";
+} from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <Navbar
       collapseOnSelect
-      expand="lg"
-      bg="primary"
-      fixed="top"
-      defaultExpanded="false"
+      expand='lg'
+      bg='primary'
+      fixed='top'
+      defaultExpanded='false'
     >
       <Container>
-        <LinkContainer to="/" style={{ color: "white" }}>
+        <LinkContainer
+          to='/'
+          style={{
+            color: 'white',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
           <Navbar.Brand>
             <FaAddressBook /> Contact Keeper
           </Navbar.Brand>
         </LinkContainer>
 
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" style={{ color: "white" }}>
-          <Nav className="ml-auto">
-            <LinkContainer to="/" style={{ color: "white" }}>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav' style={{ color: 'white' }}>
+          <Nav className='ml-auto'>
+            <LinkContainer
+              to='/'
+              style={{ color: 'white', display: 'flex', alignItems: 'center' }}
+            >
               <Nav.Link>
                 <FaHome /> Home
               </Nav.Link>
             </LinkContainer>
 
-            <Nav.Link style={{ color: "white" }} href="#about">
+            <Nav.Link
+              style={{ color: 'white', display: 'flex', alignItems: 'center' }}
+              href='#about'
+            >
               About
             </Nav.Link>
-          </Nav>
-          <Nav>
-            <LinkContainer to="/login" style={{ color: "white" }}>
+
+            <LinkContainer
+              to='/login'
+              style={{ color: 'white', display: 'flex', alignItems: 'center' }}
+            >
               <Nav.Link>
                 <FaSignInAlt /> Login
               </Nav.Link>
             </LinkContainer>
 
-            <Nav.Link style={{ color: "white" }} href="#register">
+            <Nav.Link
+              style={{ color: 'white', display: 'flex', alignItems: 'center' }}
+              href='#register'
+            >
               <FaUserPlus /> Register
             </Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link style={{ color: "white" }} href="#logout">
-              <FaSignOutAlt /> Logout
+
+            <Nav.Link
+              style={{ color: 'white', display: 'flex', alignItems: 'center' }}
+              href='#logout'
+            >
+              <FaSignOutAlt />
+              Logout
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
