54a73277ea192ff31a59f7f8e148a7a1a6626bbd
Ahmed Aly <aaoflaca@gmail.com>
2021-03-01 10:00:25 -0800

    remove restrict mode from index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ContactState from "./components/context/contact/ContactState";
-import AlertState from "./components/context/alert/AlertState";
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ContactState from './components/context/contact/ContactState';
+import AlertState from './components/context/alert/AlertState';
 
-import Header from "./components/Header";
-import About from "./screens/About";
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import AuthState from "./components/context/auth/AuthState";
-import PrivateRoute from "./components/routing/PrivateRoute";
+import Header from './components/Header';
+import About from './screens/About';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import AuthState from './components/context/auth/AuthState';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 const App = () => {
   return (
@@ -17,19 +17,19 @@ const App = () => {
       <ContactState>
         <AlertState>
           <Router>
-            <Header />
-
-            <main className="py-4">
-              <Container>
-                <Switch>
-                  <PrivateRoute path="/" component={HomeScreen} exact />
-                  <Route path="/login" component={LoginScreen} />
-                  <Route path="/register" component={RegisterScreen} />
-
-                  <Route path="/about" component={About} />
-                </Switch>
-              </Container>
-            </main>
+            <>
+              <Header />
+              <main className='py-4'>
+                <Container>
+                  <Switch>
+                    <PrivateRoute path='/' component={HomeScreen} exact />
+                    <Route path='/login' component={LoginScreen} />
+                    <Route path='/register' component={RegisterScreen} />
+                    <Route path='/about' component={About} />
+                  </Switch>
+                </Container>
+              </main>
+            </>
           </Router>
         </AlertState>
       </ContactState>
